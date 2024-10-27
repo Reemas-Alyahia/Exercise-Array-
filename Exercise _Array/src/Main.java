62b6831030ae0ec612b320d6c885521fd1ac3301
Reemas Alyahia <130400514+mime2001@users.noreply.github.com>
2024-10-27 14:42:46 +0300

    Add files via upload

---
+++ Exercise _Array/src/Main.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+////////////////////1th//////////////
+        //Write a Java program to test if the first and the last element of an array of
+        //integers are same. The length of the array must be greater than or equal to
+//        int[] number = {1, 34, 98, 44, 23, 1};
+//            if (number[0] == number[5]) {
+//                System.out.println("true");
+//            } else System.out.println("false");
+/////////////////////////2th////////////
+//        Write a Java program to find the numbers greater than the average of the
+//        numbers of a given array.
+//        int[] number = {1, 4, 17, 7, 25, 3, 100};
+//        int sum = 0;
+//        int avr = 0;
+////        The average of the said array is: 22.0 The numbers in
+////        the said array that are
+////        greater than the average are: 25 100
+//        for (int i = 0; i < number.length; i++) {
+//            sum = sum + number[i];}
+//        //
+//
+//            avr = sum / number.length;
+//            System.out.println("   The average of the said array is: " + avr);
+//            for (int n:number){
+//            if (n> avr) {
+//          System.out.println(" greater than the average are:" + n); }}
+
+
+
+
+//////////////////////////3th//////////////
+//       Write a Java program to get the larger value between first and last
+//        element of an array of integers.
+//int []number={20,30,40};//[20, 30, 40]
+//       int big=0;
+//       for(int n: number){
+//          if(n>big){
+//                big=n;
+//            }
+//       }
+//  System.out.println("the larger value:"+big);
+        ///////////////////////////////////4th/////////
+//        Write a Java program to swap the first and last elements of an array and
+////        create a new array.
+//        ArrayList <Integer> number=new ArrayList<>();
+//        number.add(20); number.add(30); number.add(40);
+//        number.set(0,40); number.set(2,20);
+//        System.out.println("the new Array is "+ number);
+        ///////////////////////////////////5th///////////
+//        Write a program that places the odd elements of an array before the
+//        even elements.
+//        Original Array:
+//[2,3,40,1,5,9,4,10,7]
+//        Sample Output:
+//[3,1,5,9,7,2,40,4,10]
+//        ArrayList <Integer> number=new ArrayList();
+//        number.add(2); number.add(3); number.add(40); number.add(1);
+//        number.add(5); number.add(9); number.add(4); number.add(10); number.add(7);
+        // ArrayList <Integer> number22=new ArrayList();
+//        System.out.println("   Original Array:"+number);
+//        ArrayList <Integer> odd=new ArrayList();
+//        for (int n:number){
+//            if (n %2!=0){
+//                odd.add(n);
+//            }
+//        }
+//        for(int n: number){
+//            if(n %2==0){
+//               odd.add(n);} }
+//
+//        System.out.println("the other Array: "+odd);
+////////////////////////////////////////6th/////////////////
+//        Write a program that test the equality of two arrays.
+//[2,3,6,6,4] [2,3,6,6,4]
+//        ArrayList<Integer> number1= new ArrayList<>();
+//        ArrayList<Integer> number2= new ArrayList<>();
+//        number1.add(2); number1.add(3);number1.add(6); number1.add(6);number1.add(4);
+//        //////////////////////////////////////
+//        number2.add(2); number2.add(3);number2.add(6); number2.add(6);number2.add(4);
+//        for(int n:number1){
+//            for(int n2: number2) {
+//                if (n == n2);
+//            }
+//        }System.out.println("True");
+
+
+    } }
